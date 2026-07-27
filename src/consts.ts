68c5f5531c99f17893b31081d5dec42c8930dde6
Son Hai Pham <psonhai.it@gmail.com>
2026-07-27 16:16:28 -0700

    feat: add header, add fullscreen button

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Astro Blog";
-export const SITE_DESCRIPTION = "Welcome to my website!";
+export const SITE_TITLE = "Tesla Home Launcher";
+export const SITE_DESCRIPTION = "A simple web app to launch favorite sites on your Tesla's browser.";
